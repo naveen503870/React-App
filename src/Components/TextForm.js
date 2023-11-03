d2ab83662a07992a27a4ad7d0bf058c64e81cb86
naveen503870 <nk738934@gmail.com>
2023-11-03 22:49:31 +0530

    Download functionality added

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -20,16 +20,26 @@ export default function TextForm(props) {
         // console.log("On change");
         setText(event.target.value);
     }
+    const downloadTxtFile = () => {
+        const element = document.createElement("a");
+        const file = new Blob([document.getElementById('input').value],    
+                    {type: 'text/plain;charset=utf-8'});
+        element.href = URL.createObjectURL(file);
+        element.download = "myFile.txt";
+        document.body.appendChild(element);
+        element.click();
+      }
     const [text, setText] = useState("");
   return (
     <>
     <div className="container">
     <h1>{props.heading}</h1>
     <div className="mb-3">
-    <textarea className="form-control" id="upper" value={text} onChange={handleOnChange} rows="3"></textarea>
+    <textarea className="form-control" id="input" value={text} onChange={handleOnChange} rows="3"></textarea>
     <button className="button btn btn-primary my-3 mx-3" onClick={handleUpClick}>Change to Uppercase</button>
     <button className="button btn btn-primary my-3 mx-3" onClick={handleDownClick}>Change to Lowercase</button>
     <button className="button btn btn-primary my-3 mx-3" onClick={handleClearClick}>Clear All</button>
+    <button className="button btn btn-primary my-3 mx-3" onClick={downloadTxtFile}>Download</button>  
 
 
     </div>
