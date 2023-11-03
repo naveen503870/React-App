bda0203e4371c6214428e072e473a571c00977b5
naveen503870 <nk738934@gmail.com>
2023-11-03 18:46:27 +0530

    word count app

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -2,21 +2,44 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=>{
-        console.log("Upper is clicked");
+        // console.log("Upper is clicked");
         let newText = text.toUpperCase();
         setText(newText)
     }
+    const handleDownClick = ()=>{
+        // console.log("Upper is clicked");
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+    const handleClearClick = ()=>{
+        // console.log("Upper is clicked");
+        let newText = '';
+        setText(newText)
+    }
     const handleOnChange = (event)=>{
-        console.log("On change");
+        // console.log("On change");
         setText(event.target.value);
     }
-    const [text, setText] = useState("Enter text here");
+    const [text, setText] = useState("");
   return (
     <>
+    <div className="container">
     <h1>{props.heading}</h1>
     <div className="mb-3">
     <textarea className="form-control" id="upper" value={text} onChange={handleOnChange} rows="3"></textarea>
-    <button className="button btn btn-primary my-3" onClick={handleUpClick}>Change to Uppercase</button>
+    <button className="button btn btn-primary my-3 mx-3" onClick={handleUpClick}>Change to Uppercase</button>
+    <button className="button btn btn-primary my-3 mx-3" onClick={handleDownClick}>Change to Lowercase</button>
+    <button className="button btn btn-primary my-3 mx-3" onClick={handleClearClick}>Clear All</button>
+
+
+    </div>
+    </div>
+    <div className="container">
+        <h1> Your word count</h1>
+        <p>{text.split(" ").length} words {text.length} character</p>
+        <p>{0.008 *text.split(" ").length } Minutes for read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
     </div>
     </>
   )
