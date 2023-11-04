49925ad8ae86f49f6faad51c7906e62e2ecc7806
naveen503870 <nk738934@gmail.com>
2023-11-04 14:20:48 +0530

    word app updted

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 // import logo from './logo.svg';
 import './App.css';
+// import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import React, { useState } from 'react'
+
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+    if(mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = "#1b1e2e";
+    }else{
+      setMode("light");
+      document.body.style.backgroundColor = "#d3d7e8";
+    }
+  }
+  
   return (
     <>
-
-<Navbar title = "ChangebyNaveen"/>
-<div className="container my-3">
-<TextForm heading="Text here"/>
-</div>
+    <Navbar title = "Word Converter" mode={mode} toggleMode={toggleMode}/>
+    <div className="container my-3">
+    <TextForm heading="Word Converter" mode={mode}/>
+    </div>
+    {/* <About/> */}
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -23,33 +23,45 @@ export default function TextForm(props) {
     const downloadTxtFile = () => {
         const element = document.createElement("a");
         const file = new Blob([document.getElementById('input').value],    
-                    {type: 'text/plain;charset=utf-8'});
+        {type: 'text/plain;charset=utf-8'});
         element.href = URL.createObjectURL(file);
         element.download = "myFile.txt";
         document.body.appendChild(element);
         element.click();
-      }
+    }
+    const handleCopy = () => {
+      var text = document.getElementById('input');
+      text.select();
+      navigator.clipboard.writeText(text.value);
+    }
+    const handleExtraSpace = () => {
+      let newText = text.split(/[ ]+/);
+      setText(newText.join(" "));
+    }
     const [text, setText] = useState("");
   return (
     <>
-    <div className="container">
+    <div className="container" style={{color: props.mode==="light"?"black":"white"}}>
     <h1>{props.heading}</h1>
+    <p className="color-grey ">A very handy online text tool where you can change between lower case and upper case letters, 
+    where you can capitalize, uncapitalize, convert to mix case and transform your text. 
+    Explore the options below:</p>
     <div className="mb-3">
-    <textarea className="form-control" id="input" value={text} onChange={handleOnChange} rows="3"></textarea>
+    <textarea className="form-control border-dark"  id="input" value={text} onChange={handleOnChange} rows="6"></textarea>
     <button className="button btn btn-primary my-3 mx-3" onClick={handleUpClick}>Change to Uppercase</button>
     <button className="button btn btn-primary my-3 mx-3" onClick={handleDownClick}>Change to Lowercase</button>
     <button className="button btn btn-primary my-3 mx-3" onClick={handleClearClick}>Clear All</button>
-    <button className="button btn btn-primary my-3 mx-3" onClick={downloadTxtFile}>Download</button>  
-
-
+    <button className="button btn btn-primary my-3 mx-3" onClick={downloadTxtFile}>Download</button> 
+    <button className="button btn btn-primary my-3 mx-3" onClick={handleCopy}>Copy Text</button> 
+    <button className="button btn btn-primary my-3 mx-3" onClick={handleExtraSpace}>Remove Extra Space</button>  
     </div>
     </div>
-    <div className="container">
+    <div className="container" style={{color: props.mode==="light"?"black":"white"}}>
         <h1> Your word count</h1>
-        <p>{text.split(" ").length} words {text.length} character</p>
-        <p>{0.008 *text.split(" ").length } Minutes for read</p>
+        <h5>{text.split(" ").length} words | {text.length} character</h5>
+        {/* <p>{0.008 * text.split(" ").length } Minutes for read</p> */}
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0 ? text : "Please write something in the text box above to preview it here" }</p>
     </div>
     </>
   )
